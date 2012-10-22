57826616972e88265af11f77c89e2735315a8401
Knight.zhou <YoungWelle@gmail.com>
2012-10-22 20:58:38 +0800

    Initial Commit

---
+++ Calendario/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Calendario
+//
+//  Created by zhou Yangbo on 12-10-22.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Calendario/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Calendario
+//
+//  Created by zhou Yangbo on 12-10-22.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
